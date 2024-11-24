2420eb7c33628a109d1dd96be074d9306d88a722
STARS1LVER <diegocardenaz11@gmail.com>
2024-11-23 19:55:50 -0500

    Optimizacion de welcome

--- src/app/features/public/welcome-page/welcome-page.component.ts
+++ src/app/features/public/welcome-page/welcome-page.component.ts
@@ -3,11 +3,13 @@ import { HeaderSharedComponent } from '../../../shared/components/header-shared/
 import { CommonModule } from '@angular/common';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { FooterSharedComponent } from '../../../shared/components/footer-shared/footer-shared.component';
+import { AboutComponent } from './components/about/about.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [ HeaderSharedComponent, CommonModule, CarrouselComponent, FooterSharedComponent ],
+  imports: [ HeaderSharedComponent, CommonModule, CarrouselComponent, FooterSharedComponent, AboutComponent, BenefitsComponent ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
